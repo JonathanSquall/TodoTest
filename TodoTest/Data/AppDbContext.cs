9c923726052da948249b343eae2f797d6b39ae81
JonathanSquall <161464204+JonathanSquall@users.noreply.github.com>
2026-03-23 14:46:25 +0100

    Add files via upload

---
+++ TodoTest/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoTest.Models;
+
+namespace TodoTest.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<TodoItem> Todos { get; set; }
+}
